a7ce1aa451f23251804cabd54719df76258336f1
Ken Yesh <kenyesh@gmail.com>
2018-02-20 23:33:56 -0500

    Think it works

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,9 +11,25 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+	d_error = 0;
+	p_error = 0.7598;
+
+	i_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+	d_error = cte - p_error;
+	p_error = cte;
+	i_error = i_error + cte;
+	if( i_error > 5){
+		i_error = 5;
+	}
+	if(i_error < 5){
+		i_error = -5;
+	}
 }
 
 double PID::TotalError() {
